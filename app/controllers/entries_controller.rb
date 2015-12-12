9b1a83e09929201460507000cf81e37920c2989a
Miguel <mmmx3m@gmail.com>
2015-12-12 12:06:00 +0100

    entries to DB working

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,4 +8,16 @@ class EntriesController < ApplicationController
   	@project = Project.find(params[:project_id])
   	@entry = @project.entries.new
   end
+  def create
+  	@project = Project.find(params[:project_id])
+  	@entry = @project.entries.new(entry_params)
+  	@entry.save
+
+  	redirect_to "/projects/#{@project.id}/entries"
+  end
+  private
+  def entry_params
+  	params.require(:entry).permit(:hours, :minutes, :date)
+  end
+
 end
